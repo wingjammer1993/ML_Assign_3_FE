6324a720e40ffc4414a76136da80bddeb047e674
Amruta <hail.radical@gmail.com>
2018-02-17 13:35:50 -0700

    data analysis

--- data_analysis.py
+++ data_analysis.py
@@ -19,21 +19,26 @@ unique_tropes = dfTrain.trope.unique()
 grouped_trope_true = x_true.groupby('trope').size().sort_values(ascending=False)
 grouped_trope_false = x_false.groupby('trope').size().sort_values(ascending=False)
 
-print(grouped_trope_true[0:10])
-print(grouped_trope_false[0:10])
+print(grouped_trope_true[0:25])
+print(grouped_trope_false[0:25])
 
 plt.xticks(rotation='vertical')
 plt.gca().set_xticklabels(grouped_trope_true.index)
-plt.plot(grouped_trope_true[0:10].values)
-plt.plot(grouped_trope_true[grouped_trope_false[0:10].index])
+l1, = plt.plot(grouped_trope_true[0:25].values, label='True')
+l2, = plt.plot(grouped_trope_true[grouped_trope_false[0:25].index], label='False')
+plt.legend(handles=[l1, l2])
 plt.tight_layout()
-
+plt.title('Top true trope distribution')
 plt.show()
+
 plt.gca().set_xticklabels(grouped_trope_false.index)
+plt.legend()
 plt.xticks(rotation='vertical')
-plt.plot(grouped_trope_false[0:10].values)
-plt.plot(grouped_trope_false[grouped_trope_true[0:10].index])
+l1, = plt.plot(grouped_trope_false[0:25].values, label='True')
+l2, = plt.plot(grouped_trope_false[grouped_trope_true[0:25].index], label='False')
+plt.legend(handles=[l1, l2])
 plt.tight_layout()
+plt.title('Top false trope distribution')
 plt.show()
 
 
